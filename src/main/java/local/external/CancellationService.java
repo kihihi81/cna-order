5fbb39b4686045027dcdcb6acb162b5eeeae1822
wini4864 <wini4864@sk.com>
2020-08-12 13:55:48 +0900

    first commit

---
+++ src/main/java/local/external/CancellationService.java
@@ -0,0 +1,19 @@
+
+package local.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="Delivery", url="http://localhost:8082")
+public interface CancellationService {
+
+    //동기호출
+
+    @RequestMapping(method= RequestMethod.GET, path="/cancellations")
+    public void cancel(@RequestBody Cancellation cancellation);
+
+}
